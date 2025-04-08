ab84eba9a43d2958c7ff77a039d01541740fc4ea
tatomchedlisvhili <tatomchedlishvili3@gmail.com>
2025-04-08 15:58:03 +0400

    GOA

---
+++ level 28/main.py
@@ -0,0 +1,37 @@
+# 1
+for i in range (20):
+    if i % 2 == 0:
+        print("even")
+    else:
+        print("odd")
+
+# 2
+ricxvi = 0
+
+while ricxvi != 20:
+    print(ricxvi)
+    ricxvi +=1
+    if ricxvi % 2 ==0:
+        print("even")
+    
+    else:
+        print("odd")
+
+
+# 3
+saxeli = input("sheiyvanet tkveni saxeli: ")
+
+if saxeli == "tato":
+    print("coincidence")
+
+else:
+    print("aremtxveva")
+
+# 4
+score = int(input("sheiyvanet tqveni qula: "))
+
+if score >= 70:
+    print("passed")
+
+else:
+    print("faild")
